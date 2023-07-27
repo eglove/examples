b722b394a4bf84f1d1b02b591eed80920c21122f
Ethan Glover <hello@ethang.email>
2023-07-26 20:14:00 -0500

    Remove MainClass component from Home page
    MainClass component was removed from the Home function in page.tsx. This was done as part of a simplification process, as MainClass was redundant and unnecessary in the current context.

--- apps/react-focus-trap/src/app/page.tsx
+++ apps/react-focus-trap/src/app/page.tsx
@@ -1,13 +1,11 @@
 import { ReactNode } from 'react';
 
 import Main from '@/app/(components)/main';
-import MainClass from '@/app/(components)/main-class';
 
 export default function Home(): ReactNode {
   return (
     <main>
       <Main />
-      <MainClass />
     </main>
   );
 }
