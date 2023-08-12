5464a433c4a35aa4610742fccaa2a4d60fdc35fd
Ethan Glover <hello@ethang.email>
2023-08-12 10:06:30 -0500

    remove tod

--- apps/java-todo/src/main/java/com/example/javatodo/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.javatodo.repository;
-
-import com.example.javatodo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, String> {
-}
