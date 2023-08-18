ec50ca1bc40680daed38b5b52bee6894effad8e9
Ethan Glover <hello@ethang.email>
2023-08-18 14:44:09 -0500

    "Implement MassTransit for event-driven communication between AuctionService & SearchService"
    A MassTransit RabbitMQ implementation has been added for event-driven asynchronous communication between the AuctionService and the SearchService.
    
    Changes include addition of:
    
    1. Three new contracts: AuctionCreated, AuctionUpdated, and AuctionDeleted for publishing events when these actions occur.
    2. The required mappings in AutoMapper profiles for these contracts.
    3. The SearchService now hosts a consumer which listens to the AuctionCreated event, maps it to an Item, and stores it.
    4. Necessary updates to the AuctionService to emit relevant events when auctions are created.
    
    Additionally, Docker Compose file, solution, and project files were updated to include and handle MassTransit and its dependencies, as well as references to the Contracts project. These changes help maintain the decoupled nature of these microservices whilst enabling asynchronous communication in the system.

--- apps/Carsties/src/SearchService/Program.cs
+++ apps/Carsties/src/SearchService/Program.cs
@@ -1,5 +1,7 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService.Consumers;
 using SearchService.Data;
 using SearchService.Services;
 
@@ -7,7 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
+
+    x.UsingRabbitMq(((context, configurator) => { configurator.ConfigureEndpoints(context); }));
+});
 
 var app = builder.Build();
 
