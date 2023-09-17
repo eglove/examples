962f8024b0884dbb81941a7e089b065cd717cdf4
Ethan Glover <hello@ethang.email>
2023-09-16 19:10:00 -0500

    Update anagram grouping algorithm to sort unicode strings
    The anagram grouping algorithm has been updated to use localeCompare() when sorting characters in a word. This is necessary for accurately grouping anagrams containing unicode characters that may not sort correctly using the default sort function. LocaleCompare() ensures that words with the same unicode characters, but different orders, are still grouped correctly as anagrams.

--- apps/algorithms/src/anagram-grouping/anagram-grouping.ts
+++ apps/algorithms/src/anagram-grouping/anagram-grouping.ts
@@ -2,7 +2,11 @@ export function anagramGrouping(words: string[]): string[][] {
   const groups = new Map<string, string[]>();
 
   for (const word of words) {
-    const sortedCharacters = [...word].toSorted().join('');
+    const sortedCharacters = [...word]
+      .toSorted((a, b) => {
+        return a.localeCompare(b);
+      })
+      .join('');
 
     if (groups.has(sortedCharacters)) {
       const group = groups.get(sortedCharacters);
