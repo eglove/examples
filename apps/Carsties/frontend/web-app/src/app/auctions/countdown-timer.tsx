06ed0fd4f0ca36def899b7f6926bd6386b2afe7d
Ethan Glover <hello@ethang.email>
2023-09-21 18:37:22 -0500

    Version Bump

--- apps/Carsties/frontend/web-app/src/app/auctions/countdown-timer.tsx
+++ apps/Carsties/frontend/web-app/src/app/auctions/countdown-timer.tsx
@@ -1,54 +1,8 @@
 'use client';
-import { DateTime } from 'luxon';
 import type { JSX } from 'react';
-import type { CountdownRenderProps } from 'react-countdown';
 import Countdown from 'react-countdown';
-import { twMerge } from 'tailwind-merge';
 
-function getTimerBackgroundColor(
-  completed: boolean,
-  days: number,
-  hours: number,
-): string {
-  if (completed) {
-    return 'bg-red-600';
-  }
-
-  if (days === 0 && hours < 10) {
-    return 'bg-amber-600';
-  }
-
-  return 'bg-green-600';
-}
-
-function Renderer({
-  hours,
-  days,
-  completed,
-  props,
-}: CountdownRenderProps): JSX.Element {
-  const dateTime = DateTime.fromJSDate(new Date(props.date));
-
-  return (
-    <div
-      className={twMerge(
-        'border-2 border-white text-white py-1 px-2 rounded-lg flex justify-center',
-        getTimerBackgroundColor(completed, days, hours),
-      )}
-    >
-      {completed ? (
-        <span>Auction Finished</span>
-      ) : (
-        <span suppressHydrationWarning>
-          Ends{' '}
-          {dateTime.toRelative({
-            style: 'long',
-          })}
-        </span>
-      )}
-    </div>
-  );
-}
+import { Renderer } from './countdown-timer-renderer';
 
 type CountdownTimerProperties = {
   readonly auctionEnd: string;
