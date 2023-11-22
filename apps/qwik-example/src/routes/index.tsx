d9f01705dac0a2a6a45119f0e9baec6104c5952e
Ethan Glover <hello@ethang.email>
2023-11-22 14:55:50 -0600

    qwik example

---
+++ apps/qwik-example/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Bug: Prefetch</h1>
+      <div>
+        <Link prefetch href={"/this-page-doesnt-exist"}>
+          Prefetched Link
+        </Link>
+      </div>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
