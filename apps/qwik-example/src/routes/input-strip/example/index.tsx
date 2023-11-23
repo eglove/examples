ff0afa7c4f5217cef5dcf5caaeceb9f78866f102
Ethan Glover <hello@ethang.email>
2023-11-23 15:49:02 -0600

    expected

--- apps/qwik-example/src/routes/input-strip/example/index.tsx
+++ apps/qwik-example/src/routes/input-strip/example/index.tsx
@@ -1,5 +1,8 @@
 import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { routeAction$ } from "@builder.io/qwik-city";
+import { atom } from "nanostores";
+
+const worldStore = atom("world");
 
 export const useAction = routeAction$((form, event) => {
   const { hello, signalValue } = form;
@@ -12,25 +15,34 @@ export const useAction = routeAction$((form, event) => {
 export default component$(() => {
   const input = useSignal<HTMLInputElement>();
   const formRef = useSignal<HTMLFormElement>();
-  const signvalValue = useSignal("world");
+
+  useVisibleTask$(() => {
+    worldStore.subscribe((value) => {
+      if (input.value) {
+        input.value.value = value;
+      }
+    });
+  });
 
   useVisibleTask$(() => {
     const handleSubmit = async (event: SubmitEvent) => {
       event.preventDefault();
-      signvalValue.value += "a";
-      console.log("client", signvalValue.value);
+      worldStore.set(`${worldStore.get()}a`);
+      console.log("client", worldStore.get());
 
       const data = new FormData();
       data.set("hello", "world");
-      data.set("signalValue", signvalValue.value);
+      data.set("signalValue", worldStore.get());
 
-      await fetch(
+      const response = await fetch(
         "http://localhost:5173/input-strip/q-data.json?qaction=Ktp7xfajduk",
         {
           method: "POST",
           body: data,
         },
       );
+
+      location.replace(response.url.split("/").slice(0, -1).join("/"));
     };
 
     formRef.value?.addEventListener("submit", handleSubmit);
@@ -38,7 +50,7 @@ export default component$(() => {
 
   return (
     <div>
-      <h1>Bug: Input Strip</h1>
+      <h1>Solution: Input Strip</h1>
       <form
         ref={formRef}
         style={{ display: "grid", width: 100, gap: 8 }}
@@ -49,7 +61,7 @@ export default component$(() => {
           ref={input}
           type={"text"}
           name={"signalValue"}
-          bind:value={signvalValue}
+          value={worldStore.get()}
         />
         <button type={"submit"}>Submit</button>
       </form>
--- apps/qwik-example/src/routes/input-strip/redirect-to/index.tsx
+++ apps/qwik-example/src/routes/input-strip/redirect-to/index.tsx
@@ -3,8 +3,9 @@ import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div>
+    <div style={{ display: "grid", gap: 8 }}>
       <Link href={"/input-strip"}>Go Back to the Bug!</Link>
+      <Link href={"/input-strip/example"}>Go Back to the Solution!</Link>
     </div>
   );
 });
