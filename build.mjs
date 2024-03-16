071a0884ebc821086d05d630689c76c0443787fd
Ethan Glover <hello@ethang.email>
2024-03-16 08:39:53 -0500

    remove clean

--- build.mjs
+++ build.mjs
@@ -2,6 +2,6 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("examples", "main", {
   preVersionBumpScripts: ["UPDATE_RECURSIVE", "PRUNE"],
-  postVersionBumpScripts: ["DEDUPE", "TURBO_CLEAN"],
+  postVersionBumpScripts: ["DEDUPE"],
   isLibrary: false,
 });
